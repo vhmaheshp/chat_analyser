8abcaf2318fa81151796589135040f04bba02cb1
ash200309 <124485267+ash200309@users.noreply.github.com>
2023-08-02 16:28:46 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -31,7 +31,7 @@ def fetch_stats(selected_user,df):
 
 def most_active_users(df):
 
-
+    df=df[df['user']!="group_notification"]
     x = df['user'].value_counts().head()
     df = round((df['user'].value_counts() / df.shape[0]) * 100, 2).reset_index().rename(
         columns={'index':'S.No.','user': 'name', 'count': 'percent'})
