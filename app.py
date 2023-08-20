203749354fe842eb28102e885a0ad22b36206964
ansh <124485267+ash200309@users.noreply.github.com>
2023-08-20 10:51:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.markdown('<h1 style="text-align: center; color: #128C7E;">{}</h1>'.format("Wh
 
 if uploaded_file is None:
     st.markdown('<h3 style="text-align: center; color: #DCF8C6;">{}</h3>'.format("Upload your whatsapp chat to see analysis on it."), unsafe_allow_html=True)
-st.markdown("<hr>", unsafe_allow_html=True)
+# st.markdown("<hr>", unsafe_allow_html=True)
 
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
@@ -192,8 +192,8 @@ if uploaded_file is not None:
                     mime='text/csv',
                     help="Download the formatted chat as a CSV file",
                     )
-        st.markdown("<hr>", unsafe_allow_html=True)
-        st.markdown('<h1 style= color: #128C7E;">{}</h1>'.format("About Author"), unsafe_allow_html=True)
-        st.markdown("**[Ansh Arora](https://www.linkedin.com/in/ansh-arora-1648a4226/)**")
+    st.markdown("<hr>", unsafe_allow_html=True)
+    st.markdown('<h1 style= color: #128C7E;">{}</h1>'.format("About Author"), unsafe_allow_html=True)
+    st.markdown("**[Ansh Arora](https://www.linkedin.com/in/ansh-arora-1648a4226/)**")
     except:
         st.markdown('<h4 style="text-align: center; color: red">{}</h4>'.format("There seems to be an error, try uploading the file with correct format."), unsafe_allow_html=True)
