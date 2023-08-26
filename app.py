d3bb878994dbe7be7c476f33b75985972e3111f5
ansh <124485267+ash200309@users.noreply.github.com>
2023-08-26 16:40:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ uploaded_file = st.sidebar.file_uploader("Select a whatsapp chat and click on ex
 if uploaded_file is None:
     st.sidebar.write("This app doesn't store or read your chats. So, you can upload them safely.")
 st.markdown('<h1 style="text-align: center; color: #128C7E;">{}</h1>'.format("WhatsApp Chat Analyzer"), unsafe_allow_html=True)
+st.sidebar.markdown("Web App by **[Ansh Arora](https://www.linkedin.com/in/ansh-arora-1648a4226/)**")
 
 if uploaded_file is None:
     st.markdown('<h3 style="text-align: center; color: #DCF8C6;">{}</h3>'.format("Upload your whatsapp chat to see analysis on it."), unsafe_allow_html=True)
@@ -194,6 +195,3 @@ if uploaded_file is not None:
                     )
     except:
         st.markdown('<h4 style="text-align: center; color: red">{}</h4>'.format("There seems to be an error, try uploading the file with correct format."), unsafe_allow_html=True)
-        st.markdown("<hr>", unsafe_allow_html=True)
-st.markdown('<h2 style= color:#128C7E;">{}</h2>'.format("About the Author"), unsafe_allow_html=True)
-st.markdown("**[Ansh Arora](https://www.linkedin.com/in/ansh-arora-1648a4226/)**")
